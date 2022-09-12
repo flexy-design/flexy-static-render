28230d49270fe7e60cb8ac3f3d169103f99828ca
hmmhmmhm <hmmhmmhm@naver.com>
2022-09-12 20:29:24 +0900

    feat: Update

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# flexy-static-render
-Cloudflare R2 + Cloudflare Worker Static Render
+# Render
+
+Proxies readonly requests to [Cloudflare R2](https://developers.cloudflare.com/r2) via [Cloudflare Workers](https://workers.dev).
+
+Note: This worker might not conform to standards very well, but it should work in most cases... if you see a bug or something missing, please please please open an issue!
+
+## Features
+- Handles `HEAD`, `GET`, and `OPTIONS` requests
+- Forwards caching headers (`etag`, `cache-control`, `expires`, `last-modified`)
+- Forwards content headers (`content-type`, `content-encoding`, `content-language`, `content-disposition`)
+- Caches served files using the [Cache API](https://developers.cloudflare.com/workers/runtime-apis/cache/)
+- Ranged requests (`range`, `if-range`, returns `content-range`)
+- Handles precondition headers (`if-modified-since`, `if-unmodified-since`, `if-match`, `if-none-match`)
+- Can serve an appended path if the requested url ends with / - Defaults to `index.html` in 0.5.0
+
+## Setup
+
+### Installing wrangler
+
+```sh
+npm i -g wrangler
+wrangler login
+```
+
+### Configuration
+
+Create your R2 bucket(s) if you haven't already (replace `bucket_name` and `preview_bucket_name` appropriately):
+```sh
+wrangler r2 bucket create bucket_name # required
+wrangler r2 bucket create preview_bucket_name # optional
+```
+You can also do this from the [Cloudflare dashboard](https://dash.cloudflare.com/?to=/:account/r2/buckets/new).
+
+Edit `wrangler.toml` to have the correct `bucket_name` and optionally, `preview_bucket_name`  (you can set it to `bucket_name`) if you're going to run this locally.
+You can do this from a fork, if using the [GitHub Actions method](#method-2-github-actions).
+
+You may edit `CACHE_CONTROL` to the default [`cache-control` header](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control) or remove it entirely to fall back to nothing. If you set `CACHE_CONTROL` to `"no-store"` then Cloudflare caching will not be used.
+
+### Deploying
+
+Note: Due to how custom domains for workers work, you MUST use a route to take advantage of caching. Cloudflare may fix this soon.
+Also note that *.workers.dev domains do not cache responses. You MUST use a route to your own (sub)domain.
+
+#### Method 1 (Local)
+```sh
+npm install
+wrangler publish # or `npm run deploy`
+```
+
+#### Method 2 (GitHub Actions)
+1. Fork this repository
+2. Set the secrets [`CF_API_TOKEN`](https://dash.cloudflare.com/profile/api-tokens) (with `Worker Scripts: Edit` permissions) and `CF_ACCOUNT_ID` in the repo settings
+3. Enable workflows in the Actions tab
+4. Update `wrangler.toml` as needed (this will trigger the workflow)
+5. (Optionally) set the worker route in the Cloudflare dashboard to use the Cache API
+
+## Using as a package
+
+You may use this worker's functionality as a package by installing and importing [`render2`](https://www.npmjs.com/package/render2):
+```sh
+npm install render2
+```
+Usage:
+```js
+import render from "render2";
+render.fetch(req, env, ctx);
+```
+
+## Development
+
+Install deps:
+```sh
+npm install
+```
+
+To launch the development server:
+```sh
+npm run dev
+```
---
+++ src/index.ts
@@ -0,0 +1,165 @@
+import parseRange from "range-parser";
+
+interface Env {
+  R2_BUCKET: R2Bucket,
+  ALLOWED_ORIGINS?: string,
+  CACHE_CONTROL?: string,
+  PATH_PREFIX?: string
+  INDEX_FILE?: string
+}
+
+type ParsedRange = { offset: number, length: number } | { suffix: number };
+
+function rangeHasLength(object: ParsedRange): object is { offset: number, length: number } {
+  return (<{ offset: number, length: number }>object).length !== undefined;
+}
+
+function hasBody(object: R2Object | R2ObjectBody): object is R2ObjectBody {
+  return (<R2ObjectBody>object).body !== undefined;
+}
+
+function hasSuffix(range: ParsedRange): range is { suffix: number } {
+  return (<{ suffix: number }>range).suffix !== undefined;
+}
+
+function getRangeHeader(range: ParsedRange, fileSize: number): string {
+  return `bytes ${hasSuffix(range) ? (fileSize - range.suffix) : range.offset}-${hasSuffix(range) ? fileSize - 1 :
+    (range.offset + range.length - 1)}/${fileSize}`;
+}
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    const allowedMethods = ["GET", "HEAD", "OPTIONS"];
+    if (allowedMethods.indexOf(request.method) === -1) return new Response("Method Not Allowed", { status: 405 });
+
+    if (request.method === "OPTIONS") {
+      return new Response(null, { headers: { "allow": allowedMethods.join(", ") } })
+    }
+
+    const url = new URL(request.url);
+    if (!env.INDEX_FILE && url.pathname === "/") {
+      return new Response("OK");
+    }
+
+    let response: Response | undefined;
+
+    const isCachingEnabled = env.CACHE_CONTROL !== "no-store"
+    const cache = caches.default;
+    if (isCachingEnabled) {
+      response = await cache.match(request);
+    }
+
+    // Since we produce this result from the request, we don't need to strictly use an R2Range
+    let range: ParsedRange | undefined;
+
+    if (!response || !response.ok) {
+      console.warn("Cache miss");
+      let path = (env.PATH_PREFIX || "") + decodeURIComponent(url.pathname.substring(1));
+
+      // Look for index file if asked for a directory
+      if (env.INDEX_FILE && (path.endsWith("/") || path === "")) {
+        path += env.INDEX_FILE;
+      }
+
+      let file: R2Object | R2ObjectBody | null | undefined;
+
+      // Range handling
+      if (request.method === "GET") {
+        const rangeHeader = request.headers.get("range");
+        if (rangeHeader) {
+          file = await env.R2_BUCKET.head(path);
+          if (file === null) return new Response("File Not Found", { status: 404 });
+          const parsedRanges = parseRange(file.size, rangeHeader);
+          // R2 only supports 1 range at the moment, reject if there is more than one
+          if (parsedRanges !== -1 && parsedRanges !== -2 && parsedRanges.length === 1 && parsedRanges.type === "bytes") {
+            let firstRange = parsedRanges[0];
+            range = file.size === (firstRange.end + 1) ? { suffix: file.size - firstRange.start } : {
+              offset: firstRange.start,
+              length: firstRange.end - firstRange.start + 1
+            }
+          } else {
+            return new Response("Range Not Satisfiable", { status: 416 });
+          }
+        }
+      }
+
+      // Etag/If-(Not)-Match handling
+      // R2 requires that etag checks must not contain quotes, and the S3 spec only allows one etag
+      // This silently ignores invalid or weak (W/) headers
+      const getHeaderEtag = (header: string | null) => header?.trim().replace(/^['"]|['"]$/g, "");
+      const ifMatch = getHeaderEtag(request.headers.get("if-match"));
+      const ifNoneMatch = getHeaderEtag(request.headers.get("if-none-match"));
+
+      const ifModifiedSince = Date.parse(request.headers.get("if-modified-since") || "");
+      const ifUnmodifiedSince = Date.parse(request.headers.get("if-unmodified-since") || "");
+
+      const ifRange = request.headers.get("if-range");
+      if (range && ifRange && file) {
+        const maybeDate = Date.parse(ifRange);
+
+        if (isNaN(maybeDate) || new Date(maybeDate) > file.uploaded) {
+          // httpEtag already has quotes, no need to use getHeaderEtag
+          if (ifRange.startsWith("W/") || ifRange !== file.httpEtag) range = undefined;
+        }
+      }
+
+      if (ifMatch || ifUnmodifiedSince) {
+        file = await env.R2_BUCKET.get(path, {
+          onlyIf: {
+            etagMatches: ifMatch,
+            uploadedBefore: ifUnmodifiedSince ? new Date(ifUnmodifiedSince) : undefined
+          }, range
+        });
+
+        if (file && !hasBody(file)) {
+          return new Response("Precondition Failed", { status: 412 });
+        }
+      }
+
+      if (ifNoneMatch || ifModifiedSince) {
+        // if-none-match overrides if-modified-since completely
+        if (ifNoneMatch) {
+          file = await env.R2_BUCKET.get(path, { onlyIf: { etagDoesNotMatch: ifNoneMatch }, range });
+        } else if (ifModifiedSince) {
+          file = await env.R2_BUCKET.get(path, { onlyIf: { uploadedAfter: new Date(ifModifiedSince) }, range });
+        }
+        if (file && !hasBody(file)) {
+          return new Response(null, { status: 304 });
+        }
+      }
+
+      file = request.method === "HEAD"
+        ? await env.R2_BUCKET.head(path)
+        : ((file && hasBody(file)) ? file : await env.R2_BUCKET.get(path, { range }));
+
+      if (file === null) {
+        return new Response("File Not Found", { status: 404 });
+      }
+
+      response = new Response((hasBody(file) && file.size !== 0) ? file.body : null, {
+        status: range ? 206 : 200,
+        headers: {
+          "accept-ranges": "bytes",
+          "access-control-allow-origin": env.ALLOWED_ORIGINS || "",
+
+          "etag": file.httpEtag,
+          "cache-control": file.httpMetadata?.cacheControl ?? (env.CACHE_CONTROL || ""),
+          "expires": file.httpMetadata?.cacheExpiry?.toUTCString() ?? "",
+          "last-modified": file.uploaded.toUTCString(),
+
+          "content-encoding": file.httpMetadata?.contentEncoding ?? "",
+          "content-type": file.httpMetadata?.contentType ?? "application/octet-stream",
+          "content-language": file.httpMetadata?.contentLanguage ?? "",
+          "content-disposition": file.httpMetadata?.contentDisposition ?? "",
+          "content-range": range ? getRangeHeader(range, file.size) : "",
+          "content-length": (range ? (rangeHasLength(range) ? range.length : range.suffix) : file.size).toString()
+        }
+      });
+
+      if (request.method === "GET" && !range && isCachingEnabled)
+        ctx.waitUntil(cache.put(request, response.clone()));
+    }
+
+    return response;
+  },
+};
